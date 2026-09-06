556dd1286bd6dcd38f68049aff2dfddc6a2bb207
MinsungKxm <minsungk101@gmail.com>
2026-09-06 11:54:22 -0400

    Added an enemy with a bullet firing system

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject projectile;
+    public float startDelay = 2f;
+    public float spawnInterval = 1.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("spawnProjectile", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnProjectile() {
+        Instantiate(projectile, transform.position, projectile.transform.rotation);
+    }
+}
